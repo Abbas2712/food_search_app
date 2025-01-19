6e671a0c1033aa19471797826c14b64184afc0b2
AbbasDexter <abbas@dexterinc.in>
2025-01-19 12:58:23 +0530

    Created Products view

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView 
+from .models import Products
+from .serializers import ProductSerializer
 # Create your views here.
+
+class ProductsView(ListCreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
