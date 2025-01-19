cc15b4d49a3bf42d41dcf0600c6e05ce31bf90ab
AbbasDexter <abbas@dexterinc.in>
2025-01-19 08:01:45 +0530

    established MySQL connection and migrated data

--- food_search_app/settings.py
+++ food_search_app/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #django packages
+    'rest_framework',
+
+    #django apps
+    'menu'
+
 ]
 
 MIDDLEWARE = [
@@ -73,10 +80,23 @@ WSGI_APPLICATION = 'food_search_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# MySQL Connection
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'food_api_schema',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
