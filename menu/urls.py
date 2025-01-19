1fda36ddc1e6682f754f8dc051827f7c5dd69c2c
AbbasDexter <abbas@dexterinc.in>
2025-01-19 14:01:00 +0530

    added retrive update delete logic to product view and registered in urls

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductsView
+from .views import ProductsView, ProductRetrieveUpdateDeleteView
 
 urlpatterns = [
-    path('/', ProductsView.as_view(), name='products'),
+    path('', ProductsView.as_view(), name='products'),
+    path('<int:pk>/', ProductRetrieveUpdateDeleteView.as_view(), name='product-detail')
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,12 @@
-from rest_framework.generics import ListCreateAPIView 
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView 
 from .models import Products
 from .serializers import ProductSerializer
 # Create your views here.
 
 class ProductsView(ListCreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
